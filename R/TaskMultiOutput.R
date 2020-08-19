10f6309a1a7972b1ac2a0dbf21fc84cc26110738
pat-s <patrick.schratz@gmail.com>
2020-08-19 10:03:07 +0200

    fix redundant task_types arg in TaskMultiOutput

--- R/TaskMultiOutput.R
+++ R/TaskMultiOutput.R
@@ -17,30 +17,30 @@ TaskMultiOutput = R6Class("TaskMultiOutput",
     #' Creates a new instance of this [R6][R6::R6Class] class.
     #' @param target (`character`)\cr
     #'   Name of the target columns.
-    #' @param task_types [`character`]\cr
+    #' @param task_type [`character`]\cr
     #'   Named character vector of per-target task-types.
     #'   E.g. c(tgt1 = "regr", tgt2 = "classif")
-    initialize = function(id, backend, target, task_types = NULL, task_type = "multiout") {
+    initialize = function(id, backend, target, task_type = "multiout") {
       super$initialize(id = id, task_type = task_type, backend = backend, target = target)
-      self$task_types = check_task_types(self, task_types) %??% infer_task_types(self)
+      self$task_type = check_task_type(self, task_type) %??% infer_task_type(self)
     },
-    #' @field task_types (`character()`)\cr
+    #' @field task_type (`character()`)\cr
     #' See `initialize`.
-    task_types = NULL
+    task_type = NULL
   )
 )
 
 # infers task types from targets via 'mlr_reflections$task_target_types' lookup
-infer_task_types = function(self) {
+infer_task_type = function(self) {
   dt = merge(self$col_info[id %in% self$target_names], mlr_reflections$task_target_types, by = "type")
   tt = setNames(dt$task_type, dt$id)
   tt[self$target_names]
 }
 
-check_task_types = function(self, task_types) {
-  if (is.null(task_types)) return(task_types)
-  assert_named(task_types, type = "unique")
-  assert_true(all(task_types %in% mlr_reflections$task_types$type))
-  assert_true(all(names(task_types) %in% self$target_names))
-  return(task_types)
+check_task_type = function(self, task_type) {
+  if (is.null(task_type)) return(task_type)
+  assert_named(task_type, type = "unique")
+  assert_true(all(task_type %in% mlr_reflections$task_type$type))
+  assert_true(all(names(task_type) %in% self$target_names))
+  return(task_type)
 }
