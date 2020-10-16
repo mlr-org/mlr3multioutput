e1934d3aef659f6fa7b785554a5c84d2d1a01ba4
Stefan Coors <stefan.coors@gmx.net>
2020-10-16 17:13:57 +0200

    new multilabel task: flags

---
+++ R/TaskMultiOutput_flags.R
@@ -0,0 +1,22 @@
+#' @title flags Task
+#'
+#' @name mlr_tasks_flags
+#' @format [R6::R6Class] inheriting from [TaskMultiOutput].
+#'
+#' @section Construction:
+#' ```
+#' mlr_tasks$get("flags")
+#' tsk("flags")
+#' ```
+#'
+#' @description
+#' A multi-output task for the [mldr.datasets::flags] data set.
+NULL
+
+load_task_flags = function(id = "flags") {
+  d = remove_named(load_dataset("flags", "mldr.datasets")$dataset, c(".labelcount", ".SCUMBLE"))
+  b = as_data_backend(d)
+  task = TaskMultiOutput$new(id, b, target = c("red", "green", "blue", "yellow", "white", "black", "orange"))
+  b$hash = task$man = "mlr3multioutput::mlr_tasks_flags"
+  task
+}
